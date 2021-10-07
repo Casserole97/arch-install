7e679e620bdf6d5a206b0df754c1a3e0bb974f20
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-07 22:55:26 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -24,3 +24,12 @@ echo 2
 echo 82
 echo w
 ) | fdisk /dev/sda
+mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+mount /dev/sda1 /mnt
+swapon /dev/sda2
+pacstrap /mnt base linux linux-firmware iwd vim grub intel-ucode man-db man-pages texinfo dhcpcd sudo
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc
