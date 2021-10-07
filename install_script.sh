7b9d13417b4f06763d9135142ff27b854e59a603
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-07 22:31:59 +0100

    added partitioning

--- install_script.sh
+++ install_script.sh
@@ -5,3 +5,22 @@ read -p "What hostname?: " new_hostname
 read -p "What username?: " new_username
 read -ps "What password?: " new_password
 timedatectl set-ntp true
+(
+echo o
+echo n
+echo p
+echo 1
+echo
+echo -$swap_amount"G"
+echo y
+echo n
+echo p
+echo 2
+echo
+echo
+echo y
+echo t
+echo 2
+echo 82
+echo w
+) | fdisk /dev/sda
