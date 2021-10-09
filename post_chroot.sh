8da318620b9382252c13c70d242e021bc6765db2
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-09 15:25:47 +0100

    Update post_chroot.sh

--- post_chroot.sh
+++ post_chroot.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
 echo "Chrooted into /mnt"
 
-#Installs more packages
-pacman -S --needed --noconfirm iwd dhcpcd grub intel-ucode vim sudo git base-devel
+#Configures pacman and installs more packages
+sed -i "s/#Color/Color\nILoveCandy/" /etc/pacman.conf
+sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5/" /etc/pacman.conf
+(
+echo [multilib]
+echo Include = /etc/pacman.d/mirrorlist
+) >> /etc/pacman.conf
+pacman -S --needed --noconfirm iwd dhcpcd grub intel-ucode vim sudo git base-devel bind nftables
 echo "Installed more packages"
 
 #Takes user input
@@ -17,7 +22,7 @@ ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 hwclock --systohc
 sed -i "s/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "Configured some things"
 
 #Hostname and password
@@ -28,7 +33,6 @@ echo 127.0.0.1        localhost
 echo ::1              localhost
 echo 127.0.1.1        $hostname
 ) >> /etc/hosts
-
 (
 echo $password
 echo $password
@@ -41,6 +45,12 @@ grub-mkconfig -o /boot/grub/grub.cfg
 echo "Installed and configured grub"
 
 #Finishing touches
-systemctl enable iwd dhcpcd
+systemctl enable iwd dhcpcd named nftables
 ip link set wlan0 up
+useradd -m $username
+(
+echo $password
+echo $password
+) | passwd $username
+echo "$username ALL=(ALL) ALL" | EDITOR="tee -a" visudo
 echo "Exiting chroot"
