d9b6cdabb3f67792d7787fe91f1ff5104fc44b5e
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-09 13:31:47 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #Takes user input
+echo "Please input these parameters"
 read -p "Hostname: " hostname
 read -p "Username: " username
 echo -s -p "Password: " password
 
 #Updates the system clock
 timedatectl set-ntp true
+echo "Updated system clock"
 
 #Partitions the disk, formats the partitions, mounts root and enables swap
 sfdisk -W always /dev/sda < sda.dump
@@ -14,15 +16,19 @@ mkfs.ext4 /dev/sda1
 mkswap /dev/sda2
 mount /dev/sda1 /mnt
 swapon /dev/sda2
+echo "Partitioned the disks and everything else"
 
 #Installs essential packages to root
 pacstrap /mnt base linux linux-firmware iwd vim grub intel-ucode man-db man-pages texinfo dhcpcd sudo base-devel git
+echo "Installed packages"
 
 #Generates an fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
+echo "Generated fstab"
 
 #Chroots into the new system
 arch-chroot /mnt
+echo "Chrooted"
 
 #Misc configurations
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
@@ -30,6 +36,7 @@ hwclock --systohc
 sed -i "s/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > locale.conf
+echo "Configured some things"
 
 #Hostname and password
 echo $hostname > /etc/hostname
@@ -44,6 +51,12 @@ echo 127.0.1.1        $hostname
 echo $password
 echo $password
 ) | passwd
+echo "Configured host stuff and password"
 
+#Bootloader
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
+echo "Installed and configured grub"
+
+#Finishing touches
+echo "Done!"
