a5fae191421389ce9456510c3d23cfd6178b2657
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-09 21:30:48 +0100

    changed to sh

--- post_chroot.sh
+++ post_chroot.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 echo "CHROOTED"
 
-#Configures pacman and installs more packages
-sed -i "s/#Color/Color\nILoveCandy/" /etc/pacman.conf
-sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5/" /etc/pacman.conf
-(
-echo [multilib]
-echo Include = /etc/pacman.d/mirrorlist
-) >> /etc/pacman.conf
-pacman -Sy --noconfirm
-pacman -S --needed --noconfirm iwd dhcpcd grub intel-ucode vim sudo git base-devel bind nftables
-echo "INSTALLED MORE PACKAGES"
-
 #Takes user input
-echo "PLEASE INPUT THESE PARAMETERS"
+echo "INPUT THESE PARAMETERS"
 read -p "Hostname: " hostname
 read -p "Username: " username
-read -s -p "Password: " password
+read -p "Password: " password
 
 #Misc configurations
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
@@ -46,8 +35,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 echo "INSTALLED AND CONFIGURED GRUB"
 
 #Finishing touches
-systemctl enable iwd dhcpcd named nftables
-ip link set wlan0 up
 useradd -m $username
 (
 echo $password
