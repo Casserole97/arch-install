7440554555cb3e18b4f441651c94d88c38fd9ce7
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-09 07:56:20 +0100

    updated the partitioning

--- install_script.sh
+++ install_script.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
 
-read -p "How much swap in GiB?: " swap_amount
-read -p "What hostname?: " new_hostname
-read -p "What username?: " new_username
-read -ps "What password?: " new_password
+read -p "Hostname: " new_hostname
+read -p "Username: " new_username
+read -ps "Password: " new_password
 timedatectl set-ntp true
-(
-echo o
-echo n
-echo p
-echo 1
-echo
-echo -$swap_amount"G"
-echo
-echo n
-echo p
-echo 2
-echo
-echo
-echo
-echo t
-echo 2
-echo 82
-echo w
-) | fdisk /dev/sda
+sfdisk /dev/sda < sda.dump
 mkfs.ext4 /dev/sda1
 mkswap /dev/sda2
 mount /dev/sda1 /mnt
