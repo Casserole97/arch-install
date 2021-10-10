481c34b853b3b1060c9969480584fa6c40227e95
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-10 15:28:14 +0100

    Update post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-git base-devel bind nftables
+username=$(logname)
 
+#Manages the wifi connection
+ip link set wlan0 up
+systemctl enable --now iwd dhcpcd named nftables
+echo "Connect to the internet:"
+iwctl
+sleep 5s
+echo "Connection (should be) estabilished!"
+
+#Configures pacman and installs more packages
 sed -i "s/#Color/Color\nILoveCandy/" /etc/pacman.conf
 sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5/" /etc/pacman.conf
 (
 echo [multilib]
 echo Include = /etc/pacman.d/mirrorlist
 ) >> /etc/pacman.conf
-pacman -Sy --noconfirm
-pacman -S --needed --noconfirm 
-echo "INSTALLED MORE PACKAGES"
+pacman -Syu --noconfirm
+pacman -S --needed --noconfirm base-devel git nvidia nvidia-utils lib32-nvidia-utils
+echo "INSTALLED MORE PACKAGES AND MODIFIED PACMAN"
 
-systemctl enable iwd dhcpcd named nftables
-ip link set wlan0 up
+#Installs paru
+sudo -u $username git clone https://aur.archlinux.org/paru.git
+cd paru
+sudo -u $username makepkg -si --noconfirm --needed
+cd
+rm -r paru
