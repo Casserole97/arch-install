42b354b93019e569370e765d17a6b223b5a341d0
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-16 17:09:51 +0100

    Update install_de.sh

--- install_de.sh
+++ install_de.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
 qtile xfce4-terminal (config it) flameshot (config it) alsa-utils (unmute sound) cozette envy-r discord firefox neofetch xdg-user-dirs
+enable autostart
 EDIT ~/.xinitrc (make it editable by default)
 
 #Sound config
