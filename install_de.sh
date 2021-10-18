86a065a63887f04d3821e141f474fd438f342530
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-18 21:08:49 +0100

    Update install_de.sh

--- install_de.sh
+++ install_de.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-paru -S xfce4 xfce4-goodies alsa-utils pulseaudio pulseaudio-alsa pavucontrol gvfs xdg-user-dirs discord firefox steam neofetch
+paru -S --noconfirm --needed xfce4 xfce4-goodies alsa-utils pulseaudio pulseaudio-alsa pavucontrol gvfs xdg-user-dirs discord firefox steam neofetch
 enable autostart
 EDIT ~/.xinitrc (make it editable by default)
