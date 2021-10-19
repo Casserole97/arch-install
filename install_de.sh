cf6e9bd50316a8f38708b1b31d22b5e66a40e90f
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-19 17:43:41 +0100

    Create install_de.sh

--- install_de.sh
+++ install_de.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 paru -S --noconfirm --needed xfce4 xfce4-goodies alsa-utils pulseaudio pulseaudio-alsa pavucontrol gvfs mugshot xdg-user-dirs discord firefox steam neofetch adobe-source-han-sans-otc-fonts redshift materia-gtk-theme kora-icon-theme ttf-roboto ttf-fira-code 
+
 EDIT ~/.xinitrc
 enable autostart
