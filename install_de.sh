8b2f16ba8fe808c731c20041f18b124bf9ff4042
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-19 16:04:02 +0100

    Update install_de.sh

--- install_de.sh
+++ install_de.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-paru -S --noconfirm --needed xfce4 xfce4-goodies alsa-utils pulseaudio pulseaudio-alsa pavucontrol gvfs mugshot xdg-user-dirs discord firefox steam neofetch noto-fonts-cjk
+paru -S --noconfirm --needed xfce4 xfce4-goodies alsa-utils pulseaudio pulseaudio-alsa pavucontrol gvfs mugshot xdg-user-dirs discord firefox steam neofetch adobe-source-han-sans-otc-fonts
 EDIT ~/.xinitrc
 enable autostart
