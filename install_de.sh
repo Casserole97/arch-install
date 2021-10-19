582766571b4a19fed7b3fafa5be38cd7ffce297f
Casserole97 <54863165+Casserole97@users.noreply.github.com>
2021-10-19 17:44:13 +0100

    Update install_de.sh

--- install_de.sh
+++ install_de.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-paru -S --noconfirm --needed xfce4 xfce4-goodies alsa-utils pulseaudio pulseaudio-alsa pavucontrol gvfs mugshot xdg-user-dirs discord firefox steam neofetch adobe-source-han-sans-otc-fonts redshift materia-gtk-theme kora-icon-theme ttf-roboto ttf-fira-code 
+paru -S --noconfirm --needed xfce4 xfce4-goodies alsa-utils pulseaudio pulseaudio-alsa pavucontrol gvfs mugshot xdg-user-dirs discord firefox steam neofetch adobe-source-han-sans-otc-fonts redshift materia-gtk-theme kora-icon-theme ttf-roboto ttf-fira-code redshift 
 
+redshift autostart
 EDIT ~/.xinitrc
 enable autostart
